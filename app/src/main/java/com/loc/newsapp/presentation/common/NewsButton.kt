00d1e38e1fe3a7d62e13db1b42eddbe755cc51ab
uday singh <>
2024-05-22 12:47:35 +1000

    Added Navigation route

--- app/src/main/java/com/loc/newsapp/presentation/common/NewsButton.kt
+++ app/src/main/java/com/loc/newsapp/presentation/common/NewsButton.kt
@@ -29,6 +29,7 @@ fun NewsButton(
         Text(
             text = text,
             style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold),
+            color = Color.White
         )
     }
 }
